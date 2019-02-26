4d6b9b31eb28d3ff9480abac46417c4771926860
Jordane Thomas <thomasjordane7@gmail.com>
2019-02-26 03:37:19 -0500

    just created the database manager script

---
+++ scripts/database_manager.py
@@ -0,0 +1,10 @@
+"""
+🌩Dreamy Jy🌩 back at it again ⚡️...
+
+This script changes the state our data base, by allowing for
+fast manipulation of the data in the database via the GraphQL API.
+
+NOTE Other Options:
+1. make a mock development api
+2. modify the DB file directily
+"""
