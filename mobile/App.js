23cd394b74a20c5e7848bdc540a22e4ee2f3d687
Jordane Thomas <thomasjordane7@gmail.com>
2019-06-14 15:18:07 -0400

    The Extended FAB's been positioned

--- mobile/App.js
+++ mobile/App.js
@@ -56,166 +56,166 @@ let testdata = {
         "dueDate": null,
         "parentId": null
       },
-      // {
-      //   "id": "7",
-      //   "title": "write an app.",
-      //   "description": null,
-      //   "dateCreated": "2019-05-21T19:57:27.334612",
-      //   "completed": true,
-      //   "dateCompleted": "2019-05-21T19:57:27.334622",
-      //   "dueDate": "2019-05-21T19:56:44",
-      //   "parentId": null
-      // },
-      // {
-      //   "id": "8",
-      //   "title": "Do my homework.",
-      //   "description": "Get your homework finished jordane!",
-      //   "dateCreated": "2019-05-21T19:57:49.757189",
-      //   "completed": false,
-      //   "dateCompleted": null,
-      //   "dueDate": null,
-      //   "parentId": null
-      // },
-      // {
-      //   "id": "9",
-      //   "title": "write an app.",
-      //   "description": null,
-      //   "dateCreated": "2019-05-21T19:57:49.757769",
-      //   "completed": true,
-      //   "dateCompleted": "2019-05-21T19:57:49.757780",
-      //   "dueDate": "2019-05-21T20:01:16.079090",
-      //   "parentId": null
-      // },
-      // {
-      //   "id": "13",
-      //   "title": "New Title",
-      //   "description": "Get your homework finished jordane!",
-      //   "dateCreated": "2019-05-21T20:02:45.188774",
-      //   "completed": false,
-      //   "dateCompleted": null,
-      //   "dueDate": "2019-05-21T20:02:35.342864",
-      //   "parentId": null
-      // },
-      // {
-      //   "id": "14",
-      //   "title": "write an app.",
-      //   "description": null,
-      //   "dateCreated": "2019-05-21T20:02:45.189563",
-      //   "completed": true,
-      //   "dateCompleted": "2019-05-21T20:02:45.189574",
-      //   "dueDate": null,
-      //   "parentId": null
-      // },
-      // {
-      //   "id": "15",
-      //   "title": "make a todo",
-      //   "description": null,
-      //   "dateCreated": "2019-05-26T03:38:59.428468",
-      //   "completed": false,
-      //   "dateCompleted": null,
-      //   "dueDate": null,
-      //   "parentId": null
-      // },
-    //   {
-    //     "id": "33",
-    //     "title": "Complete employment forms",
-    //     "description": null,
-    //     "dateCreated": "2019-05-20T07:36:13.842880",
-    //     "completed": false,
-    //     "dateCompleted": null,
-    //     "dueDate": null,
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "34",
-    //     "title": "New title",
-    //     "description": null,
-    //     "dateCreated": "2019-05-21T19:43:29.249388",
-    //     "completed": true,
-    //     "dateCompleted": "2019-05-26T03:40:50.254061",
-    //     "dueDate": null,
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "35",
-    //     "title": "Jack Black is Wack",
-    //     "description": null,
-    //     "dateCreated": "2019-05-21T19:44:37.636182",
-    //     "completed": true,
-    //     "dateCompleted": "2019-05-21T19:44:37.636197",
-    //     "dueDate": null,
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "36",
-    //     "title": "Do my homework.",
-    //     "description": "Get your homework finished jordane!",
-    //     "dateCreated": "2019-05-21T19:57:27.333206",
-    //     "completed": false,
-    //     "dateCompleted": null,
-    //     "dueDate": null,
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "37",
-    //     "title": "write an app.",
-    //     "description": null,
-    //     "dateCreated": "2019-05-21T19:57:27.334612",
-    //     "completed": true,
-    //     "dateCompleted": "2019-05-21T19:57:27.334622",
-    //     "dueDate": "2019-05-21T19:56:44",
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "38",
-    //     "title": "Do my homework.",
-    //     "description": "Get your homework finished jordane!",
-    //     "dateCreated": "2019-05-21T19:57:49.757189",
-    //     "completed": false,
-    //     "dateCompleted": null,
-    //     "dueDate": null,
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "39",
-    //     "title": "write an app.",
-    //     "description": null,
-    //     "dateCreated": "2019-05-21T19:57:49.757769",
-    //     "completed": true,
-    //     "dateCompleted": "2019-05-21T19:57:49.757780",
-    //     "dueDate": "2019-05-21T20:01:16.079090",
-    //     "parentId": null
-    //   },
-    //  {
-    //     "id": "313",
-    //     "title": "New Title",
-    //     "description": "Get your homework finished jordane!",
-    //     "dateCreated": "2019-05-21T20:02:45.188774",
-    //     "completed": false,
-    //     "dateCompleted": null,
-    //     "dueDate": "2019-05-21T20:02:35.342864",
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "314",
-    //     "title": "write an app.",
-    //     "description": null,
-    //     "dateCreated": "2019-05-21T20:02:45.189563",
-    //     "completed": true,
-    //     "dateCompleted": "2019-05-21T20:02:45.189574",
-    //     "dueDate": null,
-    //     "parentId": null
-    //   },
-    //   {
-    //     "id": "315",
-    //     "title": "make a todo",
-    //     "description": null,
-    //     "dateCreated": "2019-05-26T03:38:59.428468",
-    //     "completed": false,
-    //     "dateCompleted": null,
-    //     "dueDate": null,
-    //     "parentId": null
-    //   }
+      {
+        "id": "7",
+        "title": "write an app.",
+        "description": null,
+        "dateCreated": "2019-05-21T19:57:27.334612",
+        "completed": true,
+        "dateCompleted": "2019-05-21T19:57:27.334622",
+        "dueDate": "2019-05-21T19:56:44",
+        "parentId": null
+      },
+      {
+        "id": "8",
+        "title": "Do my homework.",
+        "description": "Get your homework finished jordane!",
+        "dateCreated": "2019-05-21T19:57:49.757189",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "9",
+        "title": "write an app.",
+        "description": null,
+        "dateCreated": "2019-05-21T19:57:49.757769",
+        "completed": true,
+        "dateCompleted": "2019-05-21T19:57:49.757780",
+        "dueDate": "2019-05-21T20:01:16.079090",
+        "parentId": null
+      },
+      {
+        "id": "13",
+        "title": "New Title",
+        "description": "Get your homework finished jordane!",
+        "dateCreated": "2019-05-21T20:02:45.188774",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": "2019-05-21T20:02:35.342864",
+        "parentId": null
+      },
+      {
+        "id": "14",
+        "title": "write an app.",
+        "description": null,
+        "dateCreated": "2019-05-21T20:02:45.189563",
+        "completed": true,
+        "dateCompleted": "2019-05-21T20:02:45.189574",
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "15",
+        "title": "make a todo",
+        "description": null,
+        "dateCreated": "2019-05-26T03:38:59.428468",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "33",
+        "title": "Complete employment forms",
+        "description": null,
+        "dateCreated": "2019-05-20T07:36:13.842880",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "34",
+        "title": "New title",
+        "description": null,
+        "dateCreated": "2019-05-21T19:43:29.249388",
+        "completed": true,
+        "dateCompleted": "2019-05-26T03:40:50.254061",
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "35",
+        "title": "Jack Black is Wack",
+        "description": null,
+        "dateCreated": "2019-05-21T19:44:37.636182",
+        "completed": true,
+        "dateCompleted": "2019-05-21T19:44:37.636197",
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "36",
+        "title": "Do my homework.",
+        "description": "Get your homework finished jordane!",
+        "dateCreated": "2019-05-21T19:57:27.333206",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "37",
+        "title": "write an app.",
+        "description": null,
+        "dateCreated": "2019-05-21T19:57:27.334612",
+        "completed": true,
+        "dateCompleted": "2019-05-21T19:57:27.334622",
+        "dueDate": "2019-05-21T19:56:44",
+        "parentId": null
+      },
+      {
+        "id": "38",
+        "title": "Do my homework.",
+        "description": "Get your homework finished jordane!",
+        "dateCreated": "2019-05-21T19:57:49.757189",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "39",
+        "title": "write an app.",
+        "description": null,
+        "dateCreated": "2019-05-21T19:57:49.757769",
+        "completed": true,
+        "dateCompleted": "2019-05-21T19:57:49.757780",
+        "dueDate": "2019-05-21T20:01:16.079090",
+        "parentId": null
+      },
+     {
+        "id": "313",
+        "title": "New Title",
+        "description": "Get your homework finished jordane!",
+        "dateCreated": "2019-05-21T20:02:45.188774",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": "2019-05-21T20:02:35.342864",
+        "parentId": null
+      },
+      {
+        "id": "314",
+        "title": "write an app.",
+        "description": null,
+        "dateCreated": "2019-05-21T20:02:45.189563",
+        "completed": true,
+        "dateCompleted": "2019-05-21T20:02:45.189574",
+        "dueDate": null,
+        "parentId": null
+      },
+      {
+        "id": "315",
+        "title": "make a todo",
+        "description": null,
+        "dateCreated": "2019-05-26T03:38:59.428468",
+        "completed": false,
+        "dateCompleted": null,
+        "dueDate": null,
+        "parentId": null
+      }
     ]
   }
 }
@@ -247,9 +247,11 @@ export default class App extends Component<Props> {
               ListFooterComponent={
                 <View>
                   <FooterView text="Guess who made me?" componentId={this.props.componentId}/>
-                  <ExtendedFloatingActionButton text="add todo" componentId={this.props.componentId}/>
                 </View>
               }/>
+        </View>
+        <View style={{position:'absolute', right: 16, bottom: 16}}>
+          <ExtendedFloatingActionButton text="add todo" componentId={this.props.componentId}/>
         </View>
       </View>
     );
@@ -279,5 +281,6 @@ const styles = StyleSheet.create({
  * 
  * ~~~Extended FAB Component
  * I don't know how to fully evaulate the effects of flex box statements.
+ * - Add shadowing and basic animation to the FAB
  * 
  */
